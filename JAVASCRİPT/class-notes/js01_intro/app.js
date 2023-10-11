200169ca3422a0f9e2c8da804da631e3b603d471
zeyneparslanj <zynp.arsln274@gmail.com>
2023-10-11 20:42:01 +0300

    mycodes

---
+++ JAVASCRİPT/class-notes/js01_intro/app.js
@@ -0,0 +1,33 @@
+// console.log("Harici APP.js kodu calisti");
+
+// document.write("DOM'a yazı bas");
+
+// karar = confirm("Devam etmek ister misini:E/H");
+// console.log(karar);
+// if (karar == true) {
+//   console.log("EVET basildi");
+// } else {
+//   console.log("HAYIR'a basildi");
+// }
+
+// console.warn("UYARI");
+
+// console.error("hata");
+
+// DEGISKEN TANIMLAMA
+
+// const kullanıırsa veri değiştirilemez ama let kullanılırsa değiştirilebilir
+
+const num1 = 5;
+let num2 = 6.6;
+
+console.log(num1);
+console.log(typeof num1);
+
+console.log(num2);
+console.log(typeof num2);
+
+// num1 = num1 + 1
+
+num2 = num2 + 1;
+console.log(num2);
